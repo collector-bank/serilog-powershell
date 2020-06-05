1ae11986b822a1b04b38c07795dcd142ef2f43a4
Per Jahn <perjahn@gmail.com>
2020-06-05 10:39:39 +0200

    Cleanup, more idiomatic powershell.

--- LogToEventhub.psm1
+++ LogToEventhub.psm1
@@ -1,22 +1,19 @@
 Set-StrictMode -v latest
 $ErrorActionPreference = "Stop"
 
-function Main()
-{
-    if (!$env:eventhubconnstr)
-    {
-        throw ("Missing environment variable: eventhubconnstr")
+function Main() {
+    if (!$env:eventhubconnstr) {
+        throw "Missing environment variable: eventhubconnstr"
     }
 
-    $serilogvariables = @{}
+    $serilogvariables = @{ }
     dir "env:/serilog.?*" | % { $serilogvariables[$_.Key.Substring(8)] = $_.Value }
 
     $global:logger = Get-Logging $env:eventhubconnstr $serilogvariables
 }
 
-function Get-Logging([string] $connstr, [Hashtable] $serilogvariables)
-{
-$csharpcode = 'using System;
+function Get-Logging([string] $connstr, [Hashtable] $serilogvariables) {
+    $csharpcode = 'using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
@@ -99,11 +96,10 @@ public class ScalarValueTypeSuffixJsonFormatter : Serilog.Formatting.Json.JsonFo
 
     Get-Dependencies $assembliesfolder
 
-    Add-Type $csharpcode -ReferencedAssemblies (Join-Path $assembliesfolder "Serilog.dll"),(Join-Path $assembliesfolder "Serilog.Formatting.Compact.dll"),"System.Linq","netstandard","System.Runtime.Extensions","System.Runtime","System.Collections","System.ObjectModel"
+    Add-Type $csharpcode -ReferencedAssemblies (Join-Path $assembliesfolder "Serilog.dll"), (Join-Path $assembliesfolder "Serilog.Formatting.Compact.dll"), "System.Linq", "netstandard", "System.Runtime.Extensions", "System.Runtime", "System.Collections", "System.ObjectModel"
 
-    if ($serilogvariables.Count -gt 0)
-    {
-$enrichercode = 'using System.Linq;
+    if ($serilogvariables.Count -gt 0) {
+        $enrichercode = 'using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -157,121 +153,107 @@ public class Enricher : ILogEventEnricher
 }'
 
 
-        Add-Type $enrichercode -ReferencedAssemblies (Join-Path $assembliesfolder "Serilog.dll"),"System.Collections","netstandard","System.Runtime.Extensions","System.Linq"
+        Add-Type $enrichercode -ReferencedAssemblies (Join-Path $assembliesfolder "Serilog.dll"), "System.Collections", "netstandard", "System.Runtime.Extensions", "System.Linq"
 
-        $logger = ([Serilog.LoggerConfiguration]::new()).Enrich.With(([Enricher]::new($serilogvariables))).WriteTo.Sink([Serilog.Sinks.AzureEventHub.AzureEventHubSink]::new(([Microsoft.Azure.EventHubs.EventHubClient]::CreateFromConnectionString($connstr)),([ScalarValueTypeSuffixJsonFormatter]::new()))).CreateLogger()
+        $logger = ([Serilog.LoggerConfiguration]::new()).Enrich.With(([Enricher]::new($serilogvariables))).WriteTo.Sink([Serilog.Sinks.AzureEventHub.AzureEventHubSink]::new(([Microsoft.Azure.EventHubs.EventHubClient]::CreateFromConnectionString($connstr)), ([ScalarValueTypeSuffixJsonFormatter]::new()))).CreateLogger()
     }
-    else
-    {
-        $logger = ([Serilog.LoggerConfiguration]::new()).WriteTo.Sink([Serilog.Sinks.AzureEventHub.AzureEventHubSink]::new(([Microsoft.Azure.EventHubs.EventHubClient]::CreateFromConnectionString($connstr)),([ScalarValueTypeSuffixJsonFormatter]::new()))).CreateLogger()
+    else {
+        $logger = ([Serilog.LoggerConfiguration]::new()).WriteTo.Sink([Serilog.Sinks.AzureEventHub.AzureEventHubSink]::new(([Microsoft.Azure.EventHubs.EventHubClient]::CreateFromConnectionString($connstr)), ([ScalarValueTypeSuffixJsonFormatter]::new()))).CreateLogger()
     }
 
     return $logger
 }
 
-function Get-Dependencies([string] $assembliesfolder)
-{
+function Get-Dependencies([string] $assembliesfolder) {
     [string[]] $nugets = `
         "Microsoft.Azure.Amqp",
-        "Microsoft.Azure.EventHubs",
-        "Serilog",
-        "Serilog.Formatting.Compact",
-        "Serilog.Sinks.AzureEventHub",
-        "Serilog.Sinks.PeriodicBatching"
+    "Microsoft.Azure.EventHubs",
+    "Serilog",
+    "Serilog.Formatting.Compact",
+    "Serilog.Sinks.AzureEventHub",
+    "Serilog.Sinks.PeriodicBatching"
 
     [string[]] $dllfiles = @($nugets | % { Get-Nuget $_ $assembliesfolder })
-    foreach ($dllfile in $dllfiles)
-    {
-        Write-Host ("Loading: '" + $dllfile + "'") -f Green
+    foreach ($dllfile in $dllfiles) {
+        Write-Host "Loading: '$dllfile'" -f Green
         Import-Module $dllfile
     }
 }
 
-function Get-Nuget([string] $packagename, [string] $assembliesfolder)
-{
-    if (!(Test-Path $assembliesfolder))
-    {
-        Write-Host ("Creating folder: '" + $assembliesfolder + "'") -f Green
+function Get-Nuget([string] $packagename, [string] $assembliesfolder) {
+    if (!(Test-Path $assembliesfolder)) {
+        Write-Host "Creating folder: '$assembliesfolder'" -f Green
         md $assembliesfolder | Out-Null
     }
 
     [string] $dllfile = Join-Path $assembliesfolder ($packagename + ".dll")
-    if (Test-Path $dllfile)
-    {
-        Write-Host ("File already downloaded: '" + $dllfile + "'") -f Green
+    if (Test-Path $dllfile) {
+        Write-Host "File already downloaded: '$dllfile'" -f Green
         return $dllfile
     }
 
     [string] $url = "https://www.nuget.org/packages/" + $packagename
 
-    Write-Host ("Downloading page: '" + $url + "'") -f Green
+    Write-Host "Downloading page: '$url'" -f Green
     [string[]] $linkrows = @(((Invoke-WebRequest $url).Content.Split("`n")) | ? { $_.Contains("Download package") })
-    if ($linkrows.Count -lt 1)
-    {
-        Write-Host ("Couldn't find any download link: '" + $url + "'") -f Yellow
+    if ($linkrows.Count -lt 1) {
+        Write-Host "Couldn't find any download link: '$url'" -f Yellow
         return
     }
-    if ($linkrows.Count -gt 1)
-    {
-        Write-Host ("Couldn't find any distinct download link: '" + $url + "'") -f Yellow
+    if ($linkrows.Count -gt 1) {
+        Write-Host "Couldn't find any distinct download link: '$url'" -f Yellow
         return
     }
     [int] $start = $linkrows[0].IndexOf('"')
-    if ($start -eq -1)
-    {
-        Write-Host ("Malformed download link: '" + $linkrows[0] + "'") -f Yellow
+    if ($start -eq -1) {
+        Write-Host "Malformed download link: '$($linkrows[0])'" -f Yellow
         return
     }
     $start++
     [int] $end = $linkrows[0].IndexOf('"', $start)
-    if ($end -eq -1)
-    {
-        Write-Host ("Malformed download link: '" + $linkrows[0] + "'") -f Yellow
+    if ($end -eq -1) {
+        Write-Host "Malformed download link: '$($linkrows[0])'" -f Yellow
         return
     }
 
-    [string] $downloadLink = $linkrows[0].Substring($start, $end-$start)
+    [string] $downloadLink = $linkrows[0].Substring($start, $end - $start)
 
     [string] $nugetfile = Join-Path $assembliesfolder ($packagename + ".nupkg")
 
-    if (Test-Path $nugetfile)
-    {
-        Write-Host ("Deleting file: '" + $nugetfile + "'") -f Green
+    if (Test-Path $nugetfile) {
+        Write-Host "Deleting file: '$nugetfile'" -f Green
         del $nugetfile
     }
 
-    Write-Host ("Downloading file: '" + $downloadLink + "' -> '" + $nugetfile + "'") -f Green
+    Write-Host "Downloading file: '$downloadLink' -> '$nugetfile'" -f Green
     Invoke-WebRequest $downloadLink -OutFile $nugetfile
 
-    if (!(Test-Path $nugetfile) -or (dir $nugetfile).Length -lt 1kb)
-    {
-        Write-Host ("Couldn't download file.") -f Yellow
+    if (!(Test-Path $nugetfile) -or (dir $nugetfile).Length -lt 1kb) {
+        Write-Host "Couldn't download file." -f Yellow
         return
     }
 
     [string] $packagefolder = Join-Path $assembliesfolder $packagename
-    if (Test-Path $packagefolder)
-    {
-        Write-Host ("Deleting folder: '" + $packagefolder + "'") -f Green
+    if (Test-Path $packagefolder) {
+        Write-Host "Deleting folder: '$packagefolder'" -f Green
         rd -Recurse -Force $packagefolder
     }
 
-    Write-Host ("Extracting: '" + $nugetfile + "' -> '" + $packagefolder + "'") -f Green
+    Write-Host "Extracting: '$nugetfile' -> '$packagefolder'" -f Green
     Expand-Archive $nugetfile -DestinationPath $packagefolder
 
-    if (Test-Path $nugetfile)
-    {
-        Write-Host ("Deleting file: '" + $nugetfile + "'") -f Green
+    if (Test-Path $nugetfile) {
+        Write-Host "Deleting file: '$nugetfile'" -f Green
         del $nugetfile
     }
 
     dir -Directory $assembliesfolder | ? { Test-Path (Join-Path $_.FullName "lib" "netstandard*" "*.dll") } | % {
         dir (Join-Path $_.FullName "lib" "netstandard*" "*.dll") | Sort-Object -Bottom 1 | % {
             [string] $nugetdllfile = $_.FullName
-            Write-Host ("Moving: '" + $nugetdllfile + "' -> '" + $assembliesfolder + "'") -f Green
+            Write-Host "Moving: '$nugetdllfile' -> '$assembliesfolder'" -f Green
             move $nugetdllfile $assembliesfolder
         }
-        Write-Host ("Deleting folder: '" + $_.FullName + "'") -f Green
+        Write-Host "Deleting folder: '$($_.FullName)'" -f Green
         rd -Recurse -Force $_.FullName
     }
 
--- test.ps1
+++ test.ps1
@@ -3,7 +3,9 @@ $ErrorActionPreference = "Stop"
 
 ${env:serilog.Author.Department}="MyDepartment"
 ${env:serilog.Author.Team}="MyTeam"
+${env:serilog.Author.System}="MySystem"
+${env:serilog.Author.Service}="MyService"
 
-Import-Module "./log_to_eventhub.psm1"
+Import-Module "./LogToEventhub.psm1"
 
 $logger.Information("hello123")
